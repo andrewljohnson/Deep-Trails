2cd8c3d3537f95ede57bcad8642fa8310f6624bb
andrewljohnson <andrew@gaiagps.com>
2016-05-19 10:27:49 -0700

    error handling for bad pickles

--- src/single_layer_network.py
+++ src/single_layer_network.py
@@ -36,6 +36,8 @@ def train_on_cached_data(raster_data_paths, neural_net_type, bands, tile_size):
 
         # read in another NAIP worth of data
         labels, images = load_training_tiles(path)
+        if not labels or not images:
+          continue
         equal_count_way_list, equal_count_tile_list = equalize_data(labels, images, False)
         new_test_labels, training_labels, new_test_images, new_training_images = \
             split_train_test(equal_count_tile_list, equal_count_way_list, .9)
