30c8caa9e31057850d32f9d9658cbd4b4515eebc
Andrew Johnson <andrew@gaiagps.com>
2016-06-10 16:42:55 -0700

    did me now too yay

--- src/s3_client_deeposm.py
+++ src/s3_client_deeposm.py
@@ -18,12 +18,11 @@ def post_findings_to_s3(raster_data_paths, model, training_info, render_results)
         if len(labels) == 0 or len(images) == 0:
             print("WARNING, there is a borked naip image file")
             continue
-        false_positives, false_negatives, fp_images, fn_images = list_findings(labels, images,
-                                                                               model)
+        false_positives, fp_images = list_findings(labels, images, model)
         path_parts = path.split('/')
         filename = path_parts[len(path_parts) - 1]
-        print("FINDINGS: {} false pos and {} false neg, of {} tiles, from {}".format(
-            len(false_positives), len(false_negatives), len(images), filename))
+        print("FINDINGS: {} false pos of {} tiles, from {}".format(
+            len(false_positives), len(images), filename))
         if render_results:
             # render JPEGs showing findings
             render_results_for_analysis([path], false_positives, fp_images, training_info['bands'],
