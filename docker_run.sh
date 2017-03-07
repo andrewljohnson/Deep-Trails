5b15b9e833e546439190a786448e96d75e0d3901
David Dean <ddean@ieee.org>
2017-03-07 01:07:29 +0000

    Remove unneeded flags from docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -21,9 +21,6 @@ fi
 
 $DOCKER run \
     -v $(pwd):/DeepOSM \
-    -w /DeepOSM \
-    -e CPLUS_INCLUDE_PATH=/usr/include/gdal \
-    -e C_INCLUDE_PATH=/usr/include/gdal \
     -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
     -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
     -it ${IMAGE_NAME} "$@"
